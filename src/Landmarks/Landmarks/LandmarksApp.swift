7d54c8c521e54bba68d14162bf9367b05ddfbaac
fxfyio <fxfyio@pm.me>
2023-08-15 12:07:53 +0800

    update

---
+++ src/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Eric on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
