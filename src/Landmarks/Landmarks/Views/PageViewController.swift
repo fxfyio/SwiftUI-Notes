445293f42212b4142c18dd36b5942910cec03fcd
fxfyio <fxfyio@pm.me>
2023-08-16 12:21:24 +0800

    和UIKit交互

---
+++ src/Landmarks/Landmarks/Views/PageViewController.swift
@@ -0,0 +1,87 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Eric on 8/16/23.
+//
+
+import SwiftUI
+import UIKit
+
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    
+    @Binding var currentPage: Int
+
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    var pages: [Page]
+
+
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+
+        pageViewController.dataSource = context.coordinator
+        
+        pageViewController.delegate = context.coordinator
+
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [context.coordinator.controllers[currentPage]], direction: .forward, animated: true)
+
+    }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return controllers.first
+            }
+            return controllers[index - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            return controllers[index + 1]
+        }
+        
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            didFinishAnimating finished: Bool,
+            previousViewControllers: [UIViewController],
+            transitionCompleted completed: Bool) {
+            if completed,
+               let visibleViewController = pageViewController.viewControllers?.first,
+               let index = controllers.firstIndex(of: visibleViewController) {
+                parent.currentPage = index
+            }
+        }
+    
+        
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+
+        init(parent: PageViewController) {
+            self.parent = parent
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+    }
+    
+}
