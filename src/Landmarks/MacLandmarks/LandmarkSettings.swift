b7b83459a60d05b96297622ee0be04c4aeaf314a
fxfyio <fxfyio@pm.me>
2023-08-17 13:42:31 +0800

    macos 设置

---
+++ src/Landmarks/MacLandmarks/LandmarkSettings.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkSettings.swift
+//  MacLandmarks
+//
+//  Created by Eric on 8/17/23.
+//
+
+import SwiftUI
+
+struct LandmarkSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkSettings_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkSettings()
+    }
+}
