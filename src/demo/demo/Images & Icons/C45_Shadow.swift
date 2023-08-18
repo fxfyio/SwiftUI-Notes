93473761c85e94ea66e41dc75f2cb6dbee9fcda2
fxfyio <fxfyio@pm.me>
2023-08-18 18:45:04 +0800

    图片阴影

---
+++ src/demo/demo/Images & Icons/C45_Shadow.swift
@@ -0,0 +1,20 @@
+//
+//  C45_Shadow.swift
+//  demo
+//
+//  Created by Eric on 8/18/23.
+//
+
+import SwiftUI
+
+struct C45_Shadow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct C45_Shadow_Previews: PreviewProvider {
+    static var previews: some View {
+        C45_Shadow()
+    }
+}
