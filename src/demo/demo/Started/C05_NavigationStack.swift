a58d5ab3153c9b6af0599518aa19596d2ad75740
fxfyio <fxfyio@pm.me>
2023-08-18 12:15:35 +0800

    导航

---
+++ src/demo/demo/Started/C05_NavigationStack.swift
@@ -0,0 +1,20 @@
+//
+//  C05_NavigationStack.swift
+//  demo
+//
+//  Created by Eric on 8/18/23.
+//
+
+import SwiftUI
+
+struct C05_NavigationStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct C05_NavigationStack_Previews: PreviewProvider {
+    static var previews: some View {
+        C05_NavigationStack()
+    }
+}
---
+++ src/demo/demo/Started/C06_ListView.swift
@@ -0,0 +1,20 @@
+//
+//  C06_ListView.swift
+//  demo
+//
+//  Created by Eric on 8/18/23.
+//
+
+import SwiftUI
+
+struct C06_ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct C06_ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        C06_ListView()
+    }
+}
