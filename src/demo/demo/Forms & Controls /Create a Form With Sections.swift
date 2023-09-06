3948d87dfd8cc4f3ead665ff6ce5fa4eaeb1a39d
fxfyio <fxfyio@pm.me>
2023-09-06 18:17:16 +0800

    text filed

---
+++ src/demo/demo/Forms & Controls /Create a Form With Sections.swift
@@ -0,0 +1,20 @@
+//
+//  Create a Form With Sections.swift
+//  demo
+//
+//  Created by Eric on 9/6/23.
+//
+
+import SwiftUI
+
+struct Create_a_Form_With_Sections: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Create_a_Form_With_Sections_Previews: PreviewProvider {
+    static var previews: some View {
+        Create_a_Form_With_Sections()
+    }
+}
